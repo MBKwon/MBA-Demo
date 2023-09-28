f6b29f552461ffb9475eb203db755a93bf78fb7f
Brad MB <maskkwon@gmail.com>
2023-09-28 23:57:48 +0900

    [no] made a view model

--- MBA-Demo/MBA-Demo/Presentation/ViewController.swift
+++ MBA-Demo/MBA-Demo/Presentation/ViewController.swift
@@ -5,13 +5,45 @@
 //  Created by Moonbeom KWON on 2023/09/28.
 //
 
+import Combine
+import MBAkit
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    
+    private(set) var viewModel = ViewModel()
+    private(set) var cancellables = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.bindViewModel(self.viewModel)
+        self.viewModel.handleMessage(.requestUserInfo)
     }
+}
 
+extension ViewController: ViewControllerConfigurable {
+    
+    typealias VM = ViewModel
+    
+    typealias I = ViewInputMessage
+    enum ViewInputMessage: InputMessage {
+        case requestUserInfo
+    }
+    
+    typealias O = ViewOutputMessage
+    enum ViewOutputMessage: OutputMessage {
+        case respondToUserInfo(userInfoList: [UserInfo])
+    }
+    
+    func bindViewModel(_ viewModel: ViewModel) {
+        viewModel.outputSubject
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: self.handleResult(_:))
+            .store(in: &self.cancellables)
+    }
+    
+    func handleResult(_ result: Result<ViewOutputMessage, Error>) {
+    }
 }
--- MBA-Demo/MBA-Demo/Presentation/ViewModel.swift
+++ MBA-Demo/MBA-Demo/Presentation/ViewModel.swift
@@ -5,4 +5,31 @@
 //  Created by Moonbeom KWON on 2023/09/28.
 //
 
+import Combine
 import Foundation
+import MBAkit
+
+class ViewModel: ViewModelConfigurable {
+    
+    typealias VC = ViewController
+    private(set) var outputSubject = PassthroughSubject<Result<VC.O, Error>, Never>()
+    
+    func handleMessage(_ inputMessage: VC.I) {
+        switch inputMessage {
+        case .requestUserInfo:
+            self.requestUserInfo()
+        }
+    }
+}
+
+extension ViewModel {
+    private func requestUserInfo() {
+        Task {
+            await APIController.shared
+                .request(path: APIController.Path.orderList, method:.get)
+                .decode(decoder: [UserInfo].self)
+                .map(VC.O.respondToUserInfo(userInfoList:))
+                .send(through: self.outputSubject)
+        }
+    }
+}
