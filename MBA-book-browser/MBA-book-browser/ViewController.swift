0909339002189898941d937850454d637fd435da
Brad MB <maskkwon@gmail.com>
2025-02-24 16:39:05 +0900

    [no] add a project for a book browser

---
+++ MBA-book-browser/MBA-book-browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MBA-book-browser
+//
+//  Created by Moonbeom KWON on 2/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
